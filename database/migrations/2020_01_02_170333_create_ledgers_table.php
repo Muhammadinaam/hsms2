7aa82f0d0f362cdd5949c7451094cd2c6eeae8cf
minaammunir <minaammunir@gmail.com>
2020-01-04 00:05:14 +0500

    added features

--- database/migrations/2020_01_02_170333_create_ledgers_table.php
+++ database/migrations/2020_01_02_170333_create_ledgers_table.php
@@ -15,10 +15,14 @@ class CreateLedgersTable extends Migration
     {
         Schema::create('ledgers', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('project_id');
+            $table->bigInteger('phase_id');
             $table->dateTime('date');
             $table->string('entry_type');
             $table->string('entry_id');
             $table->bigInteger('account_head_id');
+            $table->bigInteger('person_id')->nullable();
+            $table->bigInteger('file_id')->nullable();
             $table->string('description');
             $table->decimal('amount');
             CommonMigrations::commomColumns($table);
